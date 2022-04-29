c3193bf4d64aea12118bcf284b58a7883b025c0e
May <pakeeranam@gmail.com>
2022-04-29 22:32:33 +1000

    add nav bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,21 +2,21 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
 import About from './pages/About';
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
     return (
         <BrowserRouter>
             <div className='App'>
-                <nav>
-                    <ul>
-                        <Link to='/'>
-                            <li>Home</li>
-                        </Link>
-                        <Link to='/about'>
-                            <li>About</li>
-                        </Link>
-                    </ul>
-                </nav>
+                <Nav justify variant='tabs' defaultActiveKey='/home'>
+                    <Nav.Item>
+                        <Nav.Link href='/'>Home</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href='/about'>About</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+
                 <Routes>
                     <Route path='/' element={<HomePage />} />
                     <Route path='/about' element={<About />} />
